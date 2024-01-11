e656cf3e49f41305f167a0b433236b12e504d0a7
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-01-11 16:12:46 +0100

    Try to merge

---
+++ models/place.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# contains the `Place` class
+###########################
+from . import BaseModel
+
+
+class Place(BaseModel):
+    city_id = ""  # City.id
+    user_id = ""  # User.id
+    name = ""
+    description = ""
+    number_rooms = int(0)
+    number_bathrooms = int(0)
+    max_guest = int(0)
+    price_by_night = int(0)
+    latitude = float(0.0)
+    longitude = float(0.0)
+    amenity_ids = []  # list of Amenity.id
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
