c16304582196cd98809aa47eb19d86238f655fa2
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-01-12 04:11:35 +0100

    Fix PyCodeStyle

--- console.py
+++ console.py
@@ -22,6 +22,7 @@ inst_missing = "** no instance found **"
 attr_name_missing = "** attribute name missing **"
 attr_value_missing = "** value missing **"
 
+
 def extract(line):
     cmd, name, args = None, None, None
 
@@ -31,10 +32,10 @@ def extract(line):
         name = result.group(1)
         cmd = result.group(2)
         args = result.group(3)
-        
+
     # this next two line cleans the input
     # Example: ("test", "the", "cleaner method")
-    #        ==> 'test the "cleaner method"' 
+    #        ==> 'test the "cleaner method"'
     if args:
         args = args.replace(',', '')
         args = re.sub(r'"([^"\s]*)"', r'\1', args)
@@ -42,10 +43,6 @@ def extract(line):
     # if not match is found the return will be None, None, None
     return cmd, name, args
 
-def Not_In_Class_List(class_name):
-    if class_name in globals():
-        return False
-    return True
 
 def class_check(args):
     if len(args) == 0:
@@ -59,31 +56,34 @@ def class_check(args):
 
     return True
 
+
 def id_check(args, instances):
     if len(args) == 1:
         print(id_missing)
         return False
-    
+
     key = f"{args[0]}.{args[1]}"
     if key not in instances.keys():
         print(inst_missing)
         return False
-    
+
     return True
+
 
 def attribute_check(args):
     if len(args) == 2:
         print(attr_name_missing)
         return False
-    
+
     if args[2] in ['created_at', 'updated_at', 'id']:
         return False
-    
+
     if len(args) == 3:
         print(attr_value_missing)
         return False
-    
+
     return True
+
 
 class HBNBCommand(cmd.Cmd):
     """ the console interpreter """
@@ -93,12 +93,10 @@ class HBNBCommand(cmd.Cmd):
         """ quits the program """
         return True
 
-
     def do_EOF(self, line):
         """ exits the program """
         print()
         return True
-
 
     def emptyline(self):
         """Called when an empty line is entered in response to the prompt.
@@ -106,10 +104,10 @@ class HBNBCommand(cmd.Cmd):
         command entered."""
         pass
 
-
     def do_create(self, line):
         """ create <classname>
-        Create a new instance from the <classname> """
+            Create a new instance from the <classname>
+        """
         args = line.split()
         if not class_check(args):
             return
@@ -119,47 +117,49 @@ class HBNBCommand(cmd.Cmd):
         new_obj.save()
         print(new_obj.id)
 
-
     def do_show(self, line):
         """ show <classname> <id>
-        print string representation of the instance """
-
+            print string representation of the instance
+        """
         args = line.split()
         all_inst = storage.all()
-        if not class_check(args): return
-        if not id_check(args, all_inst): return
+        if not class_check(args):
+            return
+        if not id_check(args, all_inst):
+            return
 
         key = f"{args[0]}.{args[1]}"
         print(all_inst[key])
 
-
     def do_destroy(self, line):
         """ destroy <classname> <id>
-        deletes the given instance `can't be reverted` """
-
+            deletes the given instance `can't be reverted`
+        """
         args = line.split()
         all_inst = storage.all()
-        if not class_check(args): return
-        if not id_check(args, all_inst): return
+        if not class_check(args):
+            return
+        if not id_check(args, all_inst):
+            return
 
         key = f"{args[0]}.{args[1]}"
         all_inst.pop(key)
         storage.save()
 
-
     def do_all(self, line):
         """ all <classname>
-        shows all the instance of the <classname>/the all classes
-        if <classname> isn't specified """
-
+            shows all the instance of the <classname>/the all classes
+            if <classname> isn't specified
+        """
         all_inst = storage.all()
         args = line.split()
         obj_list = []
         if line:
-            if not class_check(args): return
-            attr = f"{line.split()[0]}\.\w+"
+            if not class_check(args):
+                return
+            attr = rf"{line.split()[0]}\.\w+"  # to get a specific class
         else:
-            attr = ".*"
+            attr = ".*"  # to get all classes
         pattern = re.compile(attr)
 
         for key, value in all_inst.items():
@@ -168,32 +168,30 @@ class HBNBCommand(cmd.Cmd):
 
         print(obj_list)
 
-
     def do_update(self, line):
         """ update <class name> <id> <attribute name> <attribute> <value>
-        updates an attribute in a specific classname by a given value"""
-
-        args = shlex.split(line)    # this splits the line respecting "quotes"
+            updates an attribute in a specific classname by a given value
+        """
+        args = shlex.split(line)  # this splits the line respecting "quotes"
         all_inst = storage.all()
-        print("all fine")
 
-        if not class_check(args): return
-        if not id_check(args, all_inst): return
-        if not attribute_check(args): return
+        if not class_check(args):
+            return
+        if not id_check(args, all_inst):
+            return
+        if not attribute_check(args):
+            return
 
         key = f"{args[0]}.{args[1]}"
         wanted_inst = all_inst[key]
         setattr(wanted_inst, args[2], args[3])
         storage.save()
 
-
     def do_count(self, name):
         """ Print the count of existing class instances """
-
         list_inst = storage.all()
         class_list = [key for key in list_inst.keys() if name in key]
         print(len(class_list))
-
 
     def default(self, line):
         """ Handles the commandes like this <class_name>.command(args) """
